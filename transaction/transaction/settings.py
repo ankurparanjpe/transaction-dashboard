49d0d23a083c561e2b091a515a8d7735e8f2e1c7
ankurparanjpe <53052533+ankurparanjpe@users.noreply.github.com>
2020-11-24 23:32:37 +0530

    Update settings.py

--- transaction/transaction/settings.py
+++ transaction/transaction/settings.py
@@ -117,5 +117,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
 STATIC_URL = '/static/'
+
+STATIC_DIRS = (
+    os.path.join(BASE_DIR, 'assets'),
+)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
